c2b3d13c6e6c08376739a3a8cefc7a696786ffdf
itsfadymate <fadybassem263@gmail.com>
2025-07-03 14:44:23 +0300

    initialised backend

---
+++ src/main/java/guc/internship/modelscrapper/ModelscrapperApplication.java
@@ -0,0 +1,13 @@
+package guc.internship.modelscrapper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModelscrapperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModelscrapperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/guc/internship/modelscrapper/ModelscrapperApplicationTests.java
@@ -0,0 +1,13 @@
+package guc.internship.modelscrapper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModelscrapperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
