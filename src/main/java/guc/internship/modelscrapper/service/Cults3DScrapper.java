6605d55c9d937655944c4de3b80641f8e62a1766
itsfadymate <fadybassem263@gmail.com>
2025-07-04 18:30:30 +0300

    I thought I commited this before cultsscrapper

---
+++ src/main/java/guc/internship/modelscrapper/service/Cults3DScrapper.java
@@ -0,0 +1,81 @@
+package guc.internship.modelscrapper.service;
+
+import guc.internship.modelscrapper.client.Cults3DApiClient;
+import guc.internship.modelscrapper.dto.Cults3DDTO;
+import guc.internship.modelscrapper.dto.Cults3DSearchResponse;
+import guc.internship.modelscrapper.model.ModelPreview;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class Cults3DScrapper implements PreviewScrapingService {
+
+    private static final Logger logger = LoggerFactory.getLogger(Cults3DScrapper.class);
+
+    @Autowired
+    private Cults3DApiClient apiClient;
+
+    @Override
+    public List<ModelPreview> scrapePreviewData(String searchTerm) {
+        logger.debug("Searching Cults3D API for: {}", searchTerm);
+        String query = String.format(
+                "{ \"query\": \"query Search { search(query: \\\"%s\\\", onlySafe: true) { " +
+                        "name(locale: EN) " +
+                        "url(locale: EN) " +
+                        "illustrationImageUrl(version: DEFAULT) " +
+                        "blueprints { fileName } " +
+                        "description(locale: EN) " +
+                        "details(locale: EN) } }\" }",
+                searchTerm
+        );
+
+
+        logger.debug("Cults3D GraphQL query: "+ query);
+        
+        try {
+
+            Cults3DSearchResponse searchResponse = apiClient.searchCults(query);
+            logger.debug("Raw API response: {}", searchResponse);
+
+            List<Cults3DDTO> response = searchResponse.getSearchResults();
+
+            if (response == null) {
+                logger.warn("getSearchResults() returned null");
+                return List.of();
+            }
+            
+
+            List<ModelPreview> previews = response.stream().map(dto ->
+                 new ModelPreview(
+                    dto.getIllustrationImageUrl(),
+                    dto.getName(),
+                    this.getSourceName(),
+                    dto.getUrl()
+                )
+            ).collect(Collectors.toList());
+            
+            logger.debug("Found {} results from Cults3D API", previews.size());
+            return previews;
+        } catch (Exception e) {
+            logger.error("Error calling Cults API for search term: {}", searchTerm, e);
+            return List.of();
+        }
+    }
+
+
+
+    @Override
+    public String getSourceName() {
+        return "Cults3D";
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
