20a0411c39f01c6437749233870ca387d66abae5
itsfadymate <fadybassem263@gmail.com>
2025-07-04 11:09:44 +0300

    Implemented the new "scrapping" functionality for ThingiverseScrapper

--- src/main/java/guc/internship/modelscrapper/service/ThingiverseScrapper.java
+++ src/main/java/guc/internship/modelscrapper/service/ThingiverseScrapper.java
@@ -1,15 +1,50 @@
 package guc.internship.modelscrapper.service;
 
+import guc.internship.modelscrapper.client.ThingiverseApiClient;
+import guc.internship.modelscrapper.dto.ThingiverseSearchResponse;
 import guc.internship.modelscrapper.model.ModelPreview;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class ThingiverseScrapper implements PreviewScrapingService{
+    
+    private static final Logger logger = LoggerFactory.getLogger(ThingiverseScrapper.class);
+    
+    @Autowired
+    private ThingiverseApiClient thingiverseApiClient;
+    
+   
+    
     @Override
     public List<ModelPreview> scrapePreviewData(String searchTerm) {
-        return List.of();
+        try {
+            logger.debug("Searching Thingiverse API for: {}", searchTerm);
+            
+            ThingiverseSearchResponse response = thingiverseApiClient.searchThings(searchTerm,"relevant" ,1);
+            
+            
+            List<ModelPreview> previews = response.getHits().stream()
+                .map((thing)->new ModelPreview(
+                                thing.getThumbnail(),
+                                thing.getName(),
+                                this.getSourceName(),
+                                thing.getPublicUrl() )
+                    )
+                .collect(Collectors.toList());
+            
+            logger.debug("Found {} results from Thingiverse API", previews.size());
+            return previews;
+            
+        } catch (Exception e) {
+            logger.error("Error calling Thingiverse API for search term: {}", searchTerm, e);
+            return List.of();
+        }
     }
 
     @Override
@@ -19,6 +54,6 @@ public class ThingiverseScrapper implements PreviewScrapingService{
 
     @Override
     public boolean isEnabled() {
-        return false;
+        return true;
     }
 }
