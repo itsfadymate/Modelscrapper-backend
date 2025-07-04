28a3c38598b85f53353000b802b078dbd82ee3ea
itsfadymate <fadybassem263@gmail.com>
2025-07-04 17:32:29 +0300

    added cult config class

---
+++ src/main/java/guc/internship/modelscrapper/client/Cults3DFeignConfig.java
@@ -0,0 +1,32 @@
+package guc.internship.modelscrapper.client;
+
+import feign.RequestInterceptor;
+import guc.internship.modelscrapper.service.ModelScrapingOrchestrator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.Base64;
+
+
+public class Cults3DFeignConfig {
+    
+    @Value("${cults3D.api.username}")
+    private String username;
+    
+    @Value("${cults3D.api.key}")
+    private String apiKey;
+
+    private static final Logger logger = LoggerFactory.getLogger(Cults3DFeignConfig.class);
+
+    @Bean
+    public RequestInterceptor cults3dRequestInterceptor() {
+        return template -> {
+            String credentials = username + ":" + apiKey;
+            String encodedCredentials = Base64.getEncoder().encodeToString(credentials.getBytes());
+            template.header("Authorization", "Basic " + encodedCredentials);
+        };
+    }
+}
