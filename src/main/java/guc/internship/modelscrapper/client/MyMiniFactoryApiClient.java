02ae7fe5a1f1957e5889ed7919faa43b97d85e76
itsfadymate <fadybassem263@gmail.com>
2025-07-04 23:00:34 +0300

    Added API Client and it's config class

---
+++ src/main/java/guc/internship/modelscrapper/client/MyMiniFactoryApiClient.java
@@ -0,0 +1,13 @@
+package guc.internship.modelscrapper.client;
+
+import guc.internship.modelscrapper.dto.MyMiniFactorySearchResponse;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient(name="MyMiniFactory-api", url="${MyMiniFactory.api.url}", configuration = MyMiniFactoryFeignConfig.class)
+public interface MyMiniFactoryApiClient {
+
+    @GetMapping("/search")
+    MyMiniFactorySearchResponse searchMiniFactory(@RequestParam(value = "q") String searchTerm);
+}
---
+++ src/main/java/guc/internship/modelscrapper/client/MyMiniFactoryFeignConfig.java
@@ -0,0 +1,33 @@
+package guc.internship.modelscrapper.client;
+
+import feign.RequestInterceptor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+
+public class MyMiniFactoryFeignConfig {
+
+    private static final Logger logger = LoggerFactory.getLogger(MyMiniFactoryFeignConfig.class);
+
+    @Value("${MyMiniFactory.api.username}")
+    private String username;
+
+    @Value("${MyMiniFactory.api.client.key}")
+    private String apiKey;
+
+    @Bean
+    public RequestInterceptor myMiniFactoryRequestInterceptor() {
+        logger.debug("MyMiniFactory username: {}", username);
+        logger.debug("MyMiniFactory API key configured: {}", apiKey != null && !apiKey.isEmpty());
+        
+        return template -> {
+            template.header("X-API-Key", apiKey);
+            template.header("X-Username", username);
+            
+            template.header("Accept", "application/json");
+            template.header("User-Agent", "ModelScrapper/1.0");
+            template.header("Connection", "keep-alive");
+        };
+    }
+}
