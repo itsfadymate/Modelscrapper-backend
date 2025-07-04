13b61d67fd859e6ca64ef0766d4ac1ae22b822b6
itsfadymate <fadybassem263@gmail.com>
2025-07-04 17:33:33 +0300

    made thingyverseconfig non global

--- src/main/java/guc/internship/modelscrapper/client/ThingiverseFeignConfig.java
+++ src/main/java/guc/internship/modelscrapper/client/ThingiverseFeignConfig.java
@@ -5,14 +5,14 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
+
 public class ThingiverseFeignConfig {
     
     @Value("${thingiverse.api.token}")
     private String apiToken;
     
     @Bean
-    public RequestInterceptor requestInterceptor() {
+    public RequestInterceptor thingiverseRequestInterceptor() {
         return template -> template.header("Authorization", "Bearer " + apiToken);
     }
 }
