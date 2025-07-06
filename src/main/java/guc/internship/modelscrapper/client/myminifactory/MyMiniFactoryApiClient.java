c7a0932b58e3d43b608db6dd59f0144aa4094fe1
itsfadymate <fadybassem263@gmail.com>
2025-07-06 16:06:30 +0300

    Added a myminifactory API call though I didn't use it

--- src/main/java/guc/internship/modelscrapper/client/myminifactory/MyMiniFactoryApiClient.java
+++ src/main/java/guc/internship/modelscrapper/client/myminifactory/MyMiniFactoryApiClient.java
@@ -1,8 +1,10 @@
 package guc.internship.modelscrapper.client.myminifactory;
 
+import guc.internship.modelscrapper.dto.myminifactory.MyMiniFactoryObjectMakes;
 import guc.internship.modelscrapper.dto.myminifactory.MyMiniFactorySearchResponse;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @FeignClient(name="MyMiniFactory-api", url="${MyMiniFactory.api.url}", configuration = MyMiniFactoryFeignConfig.class)
@@ -10,4 +12,7 @@ public interface MyMiniFactoryApiClient {
 
     @GetMapping("/search")
     MyMiniFactorySearchResponse searchMiniFactory(@RequestParam(value = "q") String searchTerm);
+
+    @GetMapping("/objects/{objectId}/prints")
+    MyMiniFactoryObjectMakes getObjectPrints(@PathVariable("objectId")String id);
 }
