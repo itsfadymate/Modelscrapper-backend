681e7b20b1aaf300b4c252bae51f1bdcb194c256
itsfadymate <fadybassem263@gmail.com>
2025-07-20 16:33:17 +0300

    Refactored ThingiverseScrapper to use composition for scrapePreviewData

--- src/main/java/guc/internship/modelscrapper/service/scraping/thingiverse/ThingiverseScrapper.java
+++ src/main/java/guc/internship/modelscrapper/service/scraping/thingiverse/ThingiverseScrapper.java
@@ -1,69 +1,29 @@
 package guc.internship.modelscrapper.service.scraping.thingiverse;
 
 import guc.internship.modelscrapper.client.thingiverse.ThingiverseApiClient;
-import guc.internship.modelscrapper.dto.thingiverse.ThingiverseSearchResponse;
 import guc.internship.modelscrapper.model.ModelPreview;
-import guc.internship.modelscrapper.service.localfilehosting.LocalFileHostingService;
+import guc.internship.modelscrapper.service.scraping.ScrapePreviewData;
 import guc.internship.modelscrapper.service.scraping.ScrapingService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.stream.Collectors;
+
 
 @Service
 public class ThingiverseScrapper implements ScrapingService {
-    
-    private static final Logger logger = LoggerFactory.getLogger(ThingiverseScrapper.class);
-    private static final int hasMakes =1;
-    private static final String type ="thing";
-    private static final String sortCriteria = "relevant";
-    
+
     @Autowired
     private ThingiverseApiClient thingiverseApiClient;
 
     @Autowired
-    private LocalFileHostingService fileHoster;
+    @Qualifier("ApiPreviewData")
+    private ScrapePreviewData previewDataScrapper;
 
     @Override
     public List<ModelPreview> scrapePreviewData(String searchTerm,boolean showFreeOnly) { //haven't found a paid model here
-        try {
-            logger.debug("Searching Thingiverse API for: {}", searchTerm);
-            
-            ThingiverseSearchResponse response = thingiverseApiClient.searchThings(searchTerm,type,hasMakes,sortCriteria);
-            
-            
-            List<ModelPreview> previews = response.getHits().stream()
-                    .filter(searchObject -> !searchObject.isNsfw())
-                    .map((searchObject)-> new ModelPreview()
-                            .setId(searchObject.getId())
-                            .setModelName(searchObject.getName())
-                            .setImageLink(
-                                    fileHoster.downloadAndRehost(
-                                            searchObject.getPreviewImage()
-                                            ,searchObject.getPreviewImage().substring(searchObject.getPreviewImage().lastIndexOf('.')
-                                            )
-                                    )
-                            )
-                            .setWebsiteLink(searchObject.getPublicUrl())
-                            .setWebsiteName(this.getSourceName())
-                            .setMakesCount(searchObject.getMakeCount())
-                            .setPrice("0")
-                            .setLikesCount(searchObject.getLikeCount())
-                            .setCommentsCount(searchObject.getCommentCount())
-                            .setFeatured(searchObject.isFeatured())
-                    )
-                    .collect(Collectors.toList());
-            
-            logger.debug("Found {} results from Thingiverse API", previews.size());
-            return previews;
-            
-        } catch (Exception e) {
-            logger.error("Error calling Thingiverse API for search term: {}", searchTerm, e);
-            return List.of();
-        }
+        return previewDataScrapper.scrapePreviewData(searchTerm,showFreeOnly);
     }
 
     @Override
