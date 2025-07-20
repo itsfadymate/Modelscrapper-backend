1572580544184828c888c32f50a81aa48016e4e7
itsfadymate <fadybassem263@gmail.com>
2025-07-20 18:41:03 +0300

    changed definition of ScrapingService to remove extension

--- src/main/java/guc/internship/modelscrapper/service/scraping/ScrapingService.java
+++ src/main/java/guc/internship/modelscrapper/service/scraping/ScrapingService.java
@@ -3,7 +3,8 @@ package guc.internship.modelscrapper.service.scraping;
 import guc.internship.modelscrapper.model.ModelPreview;
 import java.util.List;
 
-public interface ScrapingService extends ScrapePreviewData{
+public interface ScrapingService {
+    List<ModelPreview> scrapePreviewData(String searchTerm, boolean showFreeOnly,boolean useGoogleEngine);
     String getSourceName();
     List<ModelPreview.File> getDownloadLinks(String id, String downloadPageUrl);
     boolean isEnabled();
