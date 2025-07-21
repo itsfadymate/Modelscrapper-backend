6510e62c3193fa8ed30c6b55e5265612d8e2b745
itsfadymate <fadybassem263@gmail.com>
2025-07-21 15:02:22 +0300

    Modified googleClient to not take the apikey as a param

--- src/main/java/guc/internship/modelscrapper/client/google/GoogleApiClient.java
+++ src/main/java/guc/internship/modelscrapper/client/google/GoogleApiClient.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 @FeignClient(name="google-api", url="${google.api.url}")
 public interface GoogleApiClient {
   @GetMapping("customsearch/v1")
-  GoogleSearchResponse searchTerm(@RequestParam("key") String apiKey,
-                                  @RequestParam("cx") String searchEngine,
+  GoogleSearchResponse searchTerm(@RequestParam("cx") String searchEngine,
                                   @RequestParam("q") String searchTerm);
 }
