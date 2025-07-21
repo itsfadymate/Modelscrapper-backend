412d6f842f260e1e07f527969aa1ceaf6b5e772c
itsfadymate <fadybassem263@gmail.com>
2025-07-21 16:34:40 +0300

    thingiverse google strat now fetches first 4 pages

--- src/main/java/guc/internship/modelscrapper/service/scraping/thingiverse/GooglePreviewDataStrategy.java
+++ src/main/java/guc/internship/modelscrapper/service/scraping/thingiverse/GooglePreviewDataStrategy.java
@@ -22,6 +22,8 @@ public class GooglePreviewDataStrategy implements ScrapePreviewDataStrategy {
 
     private final static Logger logger = LoggerFactory.getLogger(GooglePreviewDataStrategy.class);
 
+    private final static int PAGES_TO_FETCH = 4;
+
     @Value("${google.custom.searchengine}")
     private String customSearchEngineID;
     @Autowired
@@ -34,42 +36,47 @@ public class GooglePreviewDataStrategy implements ScrapePreviewDataStrategy {
 
     @Override
     public List<ModelPreview> scrapePreviewData(String searchTerm, boolean showFreeOnly) {
-          logger.debug("scraping thingiverse preview data from google");
-          GoogleSearchResponse response = null;
-          List<String> ids = List.of();
-          try {
-              response = googleApiClient.searchTerm(customSearchEngineID,searchTerm);
-              ids = response.getLinks().stream().map(link->link.substring(link.lastIndexOf(":")+1)).toList();
-              List<ModelPreview> modelPreviews = new ArrayList<>();
-              String websiteName = new ThingiverseScrapper().getSourceName();
-              for (String id : ids){
-                  ThingiverseThing model = thingiverseApiClient.getThing(id);
-                  modelPreviews.add(new ModelPreview()
-                          .setId(model.getPublicUrl())
-                          .setModelName(model.getName())
-                          .setWebsiteLink(model.getPublicUrl())
-                          .setWebsiteName(websiteName)
-                          .setImageLink(
-                                  fileHoster.downloadAndRehost(
-                                          model.getPreviewImages()
-                                          ,model.getPreviewImages().substring(model.getPreviewImages().lastIndexOf('.')
-                                          )
-                                  )
-                          )
-                          .setPrice("0")
-                          .setFeatured(model.isFeatured())
-                          .setMakesCount(model.getMakeCount())
-                          .setLikesCount(model.getLikeCount())
-                          .setCommentsCount(model.getCommentCount())
-                          .setFiles(model.getFiles())
-                  );
-              }
-              return modelPreviews;
-          }catch (Exception e){
-              logger.debug("something went wrong while getting previewData from google {}", e.getMessage());
-              logger.debug("    google response {}",response);
-              logger.debug("    ids: {}",ids);
-              return List.of();
-          }
+        logger.debug("scraping thingiverse preview data from google");
+        GoogleSearchResponse response = null;
+        List<ModelPreview> modelPreviews = List.of();
+        List<String> ids = List.of();
+        for (int page = 0; page < PAGES_TO_FETCH; page++) {
+            try {
+                response = googleApiClient.searchTerm(customSearchEngineID, searchTerm, page * GoogleApiClient.RESULTS_PER_PAGE);
+                ids = response.getLinks().stream().map(link -> link.substring(link.lastIndexOf(":") + 1)).toList();
+                modelPreviews = new ArrayList<>();
+                String websiteName = new ThingiverseScrapper().getSourceName();
+                for (String id : ids) {
+                    ThingiverseThing model = thingiverseApiClient.getThing(id);
+                    if (model.getError()!=null)continue;
+                    modelPreviews.add(new ModelPreview()
+                            .setId(model.getPublicUrl())
+                            .setModelName(model.getName())
+                            .setWebsiteLink(model.getPublicUrl())
+                            .setWebsiteName(websiteName)
+                            .setImageLink(
+                                    fileHoster.downloadAndRehost(
+                                            model.getPreviewImages()
+                                            , model.getPreviewImages().substring(model.getPreviewImages().lastIndexOf('.')
+                                            )
+                                    )
+                            )
+                            .setPrice("0")
+                            .setFeatured(model.isFeatured())
+                            .setMakesCount(model.getMakeCount())
+                            .setLikesCount(model.getLikeCount())
+                            .setCommentsCount(model.getCommentCount())
+                            .setFiles(model.getFiles())
+                    );
+                }
+
+            } catch (Exception e) {
+                logger.debug("something went wrong while getting previewData from google {}", e.getMessage());
+                logger.debug("    google response {}", response);
+                logger.debug("    ids: {}", ids);
+
+            }
+        }
+        return modelPreviews;
     }
 }
