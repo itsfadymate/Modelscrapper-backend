f4452b143dce9c9c71a66fc3089953e828123e95
itsfadymate <fadybassem263@gmail.com>
2025-07-21 18:35:11 +0300

    refactored MyMiniFactoryScrapper to allow different search strategies

---
+++ src/main/java/guc/internship/modelscrapper/service/scraping/myminifactory/ApiPreviewDataStrategy.java
@@ -0,0 +1,54 @@
+package guc.internship.modelscrapper.service.scraping.myminifactory;
+
+import guc.internship.modelscrapper.client.myminifactory.MyMiniFactoryApiClient;
+import guc.internship.modelscrapper.dto.myminifactory.MyMiniFactorySearchResponse;
+import guc.internship.modelscrapper.model.ModelPreview;
+import guc.internship.modelscrapper.service.scraping.ScrapePreviewDataStrategy;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service("miniFactoryApiPreviewer")
+public class ApiPreviewDataStrategy implements ScrapePreviewDataStrategy {
+
+    private static final Logger logger = LoggerFactory.getLogger(ApiPreviewDataStrategy.class);
+    @Autowired
+    private MyMiniFactoryApiClient apiClient;
+
+    @Override
+    public List<ModelPreview> scrapePreviewData(String searchTerm, boolean showFreeOnly) {
+        if (showFreeOnly) return List.of(); //I am assuming all are paid
+        String websiteName = new MyMiniFactoryScrapper().getSourceName();
+        List<ModelPreview> previews = new ArrayList<>();
+        try {
+            logger.debug("Searching MyMiniFactory API for: {}", searchTerm);
+
+            MyMiniFactorySearchResponse response = apiClient.searchMiniFactory(searchTerm);
+
+            previews = response.getItems().stream()
+                    .map((dto)->new ModelPreview()
+                            .setId(String.valueOf(dto.getId()))
+                            .setImageLink(dto.getPreviewImageUrl())
+                            .setModelName(dto.getName())
+                            .setWebsiteName(websiteName)
+                            .setWebsiteLink(dto.getUrl())
+                            .setMakesCount(dto.getMakesCount())
+                            .setFiles(dto.getFiles())
+                            .setLikesCount(dto.getLikesCount())
+                            .setPrice("could be paid")
+
+                    )
+                    .collect(Collectors.toList());
+
+            logger.debug("Found {} results from MyMiniFactory API", previews.size());
+        } catch (Exception e) {
+            logger.error("Error calling MyMiniFactory API for search term: {} exception message: {}", searchTerm, e.getMessage());
+        }
+        return previews;
+    }
+}
--- src/main/java/guc/internship/modelscrapper/service/scraping/myminifactory/MyMiniFactoryScrapper.java
+++ src/main/java/guc/internship/modelscrapper/service/scraping/myminifactory/MyMiniFactoryScrapper.java
@@ -1,54 +1,29 @@
 package guc.internship.modelscrapper.service.scraping.myminifactory;
 
-import guc.internship.modelscrapper.client.myminifactory.MyMiniFactoryApiClient;
-import guc.internship.modelscrapper.dto.myminifactory.MyMiniFactorySearchResponse;
-import guc.internship.modelscrapper.model.ModelPreview;
-import guc.internship.modelscrapper.service.scraping.ScrapingService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 
+import guc.internship.modelscrapper.model.ModelPreview;
+import guc.internship.modelscrapper.service.scraping.ScrapePreviewDataStrategy;
+import guc.internship.modelscrapper.service.scraping.ScrapingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
 import java.util.List;
-import java.util.stream.Collectors;
+
 
 @Service
 public class MyMiniFactoryScrapper implements ScrapingService {
-    private static final Logger logger = LoggerFactory.getLogger(MyMiniFactoryScrapper.class);
+
     @Autowired
-    private MyMiniFactoryApiClient apiClient;
+    @Qualifier("miniFactoryGooglePreviewer")
+    private ScrapePreviewDataStrategy googlePreviewer;
+
+    @Autowired
+    @Qualifier("miniFactoryApiPreviewer")
+    private ScrapePreviewDataStrategy ApiPreviewer;
 
     @Override
     public List<ModelPreview> scrapePreviewData(String searchTerm,boolean showFreeOnly,boolean useGoogleEngine) {
-        if (showFreeOnly) return List.of(); //I am assuming all are paid
-        try {
-            logger.debug("Searching MyMiniFactory API for: {}", searchTerm);
-
-            MyMiniFactorySearchResponse response = apiClient.searchMiniFactory(searchTerm);
-
-
-            List<ModelPreview> previews = response.getItems().stream()
-                    .map((dto)->new ModelPreview()
-                            .setId(String.valueOf(dto.getId()))
-                            .setImageLink(dto.getPreviewImageUrl())
-                            .setModelName(dto.getName())
-                            .setWebsiteName(this.getSourceName())
-                            .setWebsiteLink(dto.getUrl())
-                            .setMakesCount(dto.getMakesCount())
-                            .setFiles(dto.getFiles())
-                            .setLikesCount(dto.getLikesCount())
-                            .setPrice("could be paid")
-
-                    )
-                    .collect(Collectors.toList());
-
-            logger.debug("Found {} results from MyMiniFactory API", previews.size());
-            return previews;
-
-        } catch (Exception e) {
-            logger.error("Error calling MyMiniFactory API for search term: {}", searchTerm, e);
-            return List.of();
-        }
+        return useGoogleEngine? googlePreviewer.scrapePreviewData(searchTerm,showFreeOnly) : ApiPreviewer.scrapePreviewData(searchTerm,showFreeOnly);
     }
 
     @Override
