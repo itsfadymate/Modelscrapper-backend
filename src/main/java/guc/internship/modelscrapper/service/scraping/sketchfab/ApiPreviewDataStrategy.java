36eb2017a809fb1b7f760bdfdb1f8ac911651fc5
itsfadymate <fadybassem263@gmail.com>
2025-07-21 20:17:10 +0300

    refactored SketchfabScrapper

---
+++ src/main/java/guc/internship/modelscrapper/service/scraping/sketchfab/ApiPreviewDataStrategy.java
@@ -0,0 +1,49 @@
+package guc.internship.modelscrapper.service.scraping.sketchfab;
+
+import guc.internship.modelscrapper.client.sketchfab.SketchfabApiClient;
+import guc.internship.modelscrapper.dto.sketchfab.SketchfabSearchResponse;
+import guc.internship.modelscrapper.model.ModelPreview;
+import guc.internship.modelscrapper.service.scraping.ScrapePreviewDataStrategy;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+
+import java.util.List;
+
+
+@Service("sketchFabApiPreviewer")
+public class ApiPreviewDataStrategy implements ScrapePreviewDataStrategy {
+
+    private static final Logger logger = LoggerFactory.getLogger(ApiPreviewDataStrategy.class);
+    @Autowired
+    private SketchfabApiClient apiClient;
+
+    @Override
+    public List<ModelPreview> scrapePreviewData(String searchTerm, boolean showFreeOnly) {
+        logger.debug("getting sketchfab api previewData");
+        String websiteName = new SketchfabScrapper().getSourceName();
+        try {
+            SketchfabSearchResponse searchResponse = apiClient.searchSketchfab(searchTerm, showFreeOnly);
+
+            return searchResponse.getResults().stream()
+                    .filter(o -> !o.isNsfw())
+                    .map(dto -> new ModelPreview()
+                            .setId(dto.getId())
+                            .setModelName(dto.getName())
+                            .setImageLink(dto.getPreviewImageUrl())
+                            .setWebsiteName(websiteName)
+                            .setWebsiteLink(dto.getUrl())
+                            .setCommentsCount(dto.getCommentCount())
+                            .setLikesCount(dto.getLikesCount())
+                            .setFeatured(dto.isFeatured())
+                            .setPrice(dto.isDownloadable() ? "0" : "paid")
+                    ).toList();
+        } catch (Exception e) {
+            logger.warn("An error occurred while scrapping sketchfab {}", e.getMessage());
+            return List.of();
+
+        }
+    }
+}
--- src/main/java/guc/internship/modelscrapper/service/scraping/sketchfab/SketchfabScrapper.java
+++ src/main/java/guc/internship/modelscrapper/service/scraping/sketchfab/SketchfabScrapper.java
@@ -3,9 +3,11 @@ package guc.internship.modelscrapper.service.scraping.sketchfab;
 import guc.internship.modelscrapper.client.sketchfab.SketchfabApiClient;
 import guc.internship.modelscrapper.dto.sketchfab.SketchfabSearchResponse;
 import guc.internship.modelscrapper.model.ModelPreview;
+import guc.internship.modelscrapper.service.scraping.ScrapePreviewDataStrategy;
 import guc.internship.modelscrapper.service.scraping.ScrapingService;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -17,32 +19,18 @@ import org.slf4j.Logger;
 public class SketchfabScrapper implements ScrapingService {
 
     @Autowired
-    private SketchfabApiClient apiClient;
+    @Qualifier("sketchFabGooglePreviewer")
+    private ScrapePreviewDataStrategy googlePreviewer;
+
+    @Autowired
+    @Qualifier("sketchFabApiPreviewer")
+    private ScrapePreviewDataStrategy ApiPreviewer;
 
     private static final Logger logger = LoggerFactory.getLogger(SketchfabScrapper.class);
 
     @Override
     public List<ModelPreview> scrapePreviewData(String searchTerm, boolean showFreeOnly,boolean useGoogleEngine) {
-        try{
-            SketchfabSearchResponse searchResponse = apiClient.searchSketchfab(searchTerm,showFreeOnly);
-
-            return searchResponse.getResults().stream()
-                    .filter(o->!o.isNsfw())
-                    .map(dto->new ModelPreview()
-                            .setId(dto.getId())
-                            .setModelName(dto.getName())
-                            .setImageLink(dto.getPreviewImageUrl())
-                            .setWebsiteName(this.getSourceName())
-                            .setWebsiteLink(dto.getUrl())
-                            .setCommentsCount(dto.getCommentCount())
-                            .setLikesCount(dto.getLikesCount())
-                            .setFeatured(dto.isFeatured())
-                            .setPrice(dto.isDownloadable()? "0":"paid")
-                    ).toList();
-        } catch (Exception e) {
-            logger.warn("An error occured while scrapping sketchfab {}",e.getMessage());
-            return List.of();
-        }
+        return useGoogleEngine?googlePreviewer.scrapePreviewData(searchTerm,showFreeOnly) : ApiPreviewer.scrapePreviewData(searchTerm,showFreeOnly);
     }
 
     @Override
