8277580982c559ba15998daa6ec2df18a2fb71d6
itsfadymate <fadybassem263@gmail.com>
2025-07-22 16:56:53 +0300

    forgot to add those

--- src/main/java/guc/internship/modelscrapper/service/scraping/printables/GooglePreviewDataStrategy.java
+++ src/main/java/guc/internship/modelscrapper/service/scraping/printables/GooglePreviewDataStrategy.java
@@ -2,9 +2,11 @@ package guc.internship.modelscrapper.service.scraping.printables;
 
 import guc.internship.modelscrapper.model.ModelPreview;
 import guc.internship.modelscrapper.service.scraping.ScrapePreviewDataStrategy;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service("GooglePrintables")
 public class GooglePreviewDataStrategy implements ScrapePreviewDataStrategy {
     @Override
     public List<ModelPreview> scrapePreviewData(String searchTerm, boolean showFreeOnly) {
--- src/main/java/guc/internship/modelscrapper/service/scraping/printables/PlaywrightPreviewDataStrategy.java
+++ src/main/java/guc/internship/modelscrapper/service/scraping/printables/PlaywrightPreviewDataStrategy.java
@@ -12,10 +12,12 @@ import org.jsoup.select.Elements;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Service("PlaywrightPrintables")
 public class PlaywrightPreviewDataStrategy implements ScrapePreviewDataStrategy {
 
     private static final Logger logger = LoggerFactory.getLogger(PlaywrightPreviewDataStrategy.class);
