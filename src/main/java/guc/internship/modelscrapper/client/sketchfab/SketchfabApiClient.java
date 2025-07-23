8c8390c845b403e793353a0c5c0b54d5c1afa2af
itsfadymate <fadybassem263@gmail.com>
2025-07-23 14:01:31 +0300

    Added a get model method to SketchfabApiClient

--- src/main/java/guc/internship/modelscrapper/client/sketchfab/SketchfabApiClient.java
+++ src/main/java/guc/internship/modelscrapper/client/sketchfab/SketchfabApiClient.java
@@ -1,8 +1,10 @@
 package guc.internship.modelscrapper.client.sketchfab;
 
+import guc.internship.modelscrapper.dto.sketchfab.SketchfabSearchObject;
 import guc.internship.modelscrapper.dto.sketchfab.SketchfabSearchResponse;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @FeignClient(name="sketchfab-api", url="${Sketchfab.api.url}")
@@ -11,4 +13,7 @@ public interface SketchfabApiClient {
     @GetMapping("/search?type=models")
     SketchfabSearchResponse searchSketchfab(@RequestParam("q") String searchTerm,
                                             @RequestParam("downloadable") boolean downloadable);
+
+    @GetMapping("/models/{uid}")
+    SketchfabSearchObject getModel(@PathVariable("uid") String uid);
 }
