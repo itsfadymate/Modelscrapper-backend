acafe6646e8e26494da30ab6a055e0ef4e7fb924
itsfadymate <fadybassem263@gmail.com>
2025-07-28 14:18:26 +0300

    Implemented getDownloadLinks for MyMiniFactoryScrapper

--- src/main/java/guc/internship/modelscrapper/service/scraping/myminifactory/MyMiniFactoryScrapper.java
+++ src/main/java/guc/internship/modelscrapper/service/scraping/myminifactory/MyMiniFactoryScrapper.java
@@ -1,9 +1,12 @@
 package guc.internship.modelscrapper.service.scraping.myminifactory;
 
 
+import guc.internship.modelscrapper.client.myminifactory.MyMiniFactoryApiClient;
 import guc.internship.modelscrapper.model.ModelPreview;
 import guc.internship.modelscrapper.service.scraping.ScrapePreviewDataStrategy;
 import guc.internship.modelscrapper.service.scraping.ScrapingService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
@@ -13,6 +16,9 @@ import java.util.List;
 @Service
 public class MyMiniFactoryScrapper implements ScrapingService {
 
+    private static final Logger logger = LoggerFactory.getLogger(MyMiniFactoryScrapper.class);
+
+
     @Autowired
     @Qualifier("miniFactoryGooglePreviewer")
     private ScrapePreviewDataStrategy googlePreviewer;
@@ -20,6 +26,9 @@ public class MyMiniFactoryScrapper implements ScrapingService {
     @Autowired
     @Qualifier("miniFactoryApiPreviewer")
     private ScrapePreviewDataStrategy ApiPreviewer;
+
+    @Autowired
+    private MyMiniFactoryApiClient apiClient;
 
     @Override
     public List<ModelPreview> scrapePreviewData(String searchTerm,boolean showFreeOnly,boolean useGoogleEngine) {
@@ -33,7 +42,12 @@ public class MyMiniFactoryScrapper implements ScrapingService {
 
     @Override
     public List<ModelPreview.File> getDownloadLinks(String id, String downloadPageUrl) {
-        //TODO: something along the lines of getting the download links yada-yada
+        logger.debug("getting download links for {} with ID {} downloadPage {}",this.getSourceName(),id,downloadPageUrl);
+        try {
+            return apiClient.getObject(id).getFiles();
+        }catch (Exception e){
+            logger.error("something went wrong while getting download links for myMiniFactory");
+        }
         return List.of();
     }
 
