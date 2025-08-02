90ae22b93e00f5452c8aa7cbf569aa86d92136bb
itsfadymate <fadybassem263@gmail.com>
2025-08-02 09:14:47 +0300

    changed signature of interface to take filter objects

--- src/main/java/guc/internship/modelscrapper/service/scraping/ScrapePreviewDataStrategy.java
+++ src/main/java/guc/internship/modelscrapper/service/scraping/ScrapePreviewDataStrategy.java
@@ -2,8 +2,9 @@ package guc.internship.modelscrapper.service.scraping;
 
 import java.util.List;
 
+import guc.internship.modelscrapper.dto.search.SearchFilter;
 import guc.internship.modelscrapper.model.ModelPreview;
 
 public interface ScrapePreviewDataStrategy {
-    List<ModelPreview> scrapePreviewData(String searchTerm, boolean showFreeOnly, String websiteName);
+    List<ModelPreview> scrapePreviewData(String searchTerm, SearchFilter filter, String websiteName);
 }
--- src/main/java/guc/internship/modelscrapper/service/scraping/ScrapingService.java
+++ src/main/java/guc/internship/modelscrapper/service/scraping/ScrapingService.java
@@ -1,11 +1,12 @@
 package guc.internship.modelscrapper.service.scraping;
 
+import guc.internship.modelscrapper.dto.search.SearchFilter;
 import guc.internship.modelscrapper.model.ModelDetails;
 import guc.internship.modelscrapper.model.ModelPreview;
 import java.util.List;
 
 public interface ScrapingService {
-    List<ModelPreview> scrapePreviewData(String searchTerm, boolean showFreeOnly,boolean useGoogleEngine);
+    List<ModelPreview> scrapePreviewData(String searchTerm, SearchFilter filter);
     String getSourceName();
     List<ModelPreview.File> getDownloadLinks(String id, String downloadPageUrl);
     ModelDetails getModelDetails(String id,String downloadPageUrl);
